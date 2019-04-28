0657808be5095395e5f9b6b5065bb812c14c23ed
HarrisunW <47761871+HarrisunW@users.noreply.github.com>
2019-04-28 14:23:17 +1000

    Update CentralityMeasures.c

--- CentralityMeasures.c
+++ CentralityMeasures.c
@@ -4,38 +4,53 @@
 #include "PQ.h"
 #include <stdlib.h>
 #include <stdio.h>
-#include <assert.h>
+
+// The data type of counting number of times passing through v and shortest paths tracking
+typedef struct Betweeness {
+    double counterV; 
+    double counterP; 
+} Betweeness;
 
 static NodeValues newNode(int nV);
+static Betweeness countShortestPaths(ShortestPaths paths, Betweeness bw, int src, int dest, int v); 
 
+// The number of ties that the node directs to others
 NodeValues outDegreeCentrality(Graph g) {
     if (g == NULL) {
         NodeValues throwAway = {0};
         return throwAway;
     }
     NodeValues out = newNode(numVerticies(g));
+    int count;
     for (int i = 0; i < numVerticies(g); i++) {
+        count = 0;
         for (AdjList curr = outIncident(g,i); curr != NULL; curr = curr->next) {
-            out.values[i] = out.values[i] + 1;
+            count++;
+            out.values[i] = count;
         }
     }
     return out;
 }
 
+// A count of the number of ties directed to the node
 NodeValues inDegreeCentrality(Graph g) {
     if (g == NULL) {
         NodeValues throwAway = {0};
         return throwAway;
     }
     NodeValues in = newNode(numVerticies(g));
+    int count;
     for (int i = 0; i < numVerticies(g); i++) {
+        count = 0;
         for (AdjList curr = inIncident(g,i); curr != NULL; curr = curr->next) {
-            in.values[i] = in.values[i] + 1;
+            count++;
+            in.values[i] = count;
         }
     }
     return in;
 }
 
+// The total number of indegree centrality and outdegree centrality
 NodeValues degreeCentrality(Graph g) {
     if (g == NULL) {
         NodeValues throwAway = {0};
@@ -48,6 +63,7 @@ NodeValues degreeCentrality(Graph g) {
     return dc;
 }
 
+// Calculate the sum of the length of the shortest paths between the node and all other nodes
 NodeValues closenessCentrality(Graph g) {
     if (g == NULL) {
         NodeValues throwAway = {0};
@@ -79,45 +95,48 @@ NodeValues closenessCentrality(Graph g) {
     return closeness;
 }
 
-NodeValues betweennessCentrality(Graph g){
-    if (g == NULL) {
+// The total number of shortest paths between two different nodes.
+NodeValues betweennessCentrality(Graph g) {
+	if (g == NULL) {
         NodeValues throwAway = {0};
         return throwAway;
     }
-    int count1, count2;
-    double value;
-    NodeValues betweenness = newNode(numVerticies(g));
-    for (int i = 0; i < numVerticies(g); i++) {
-        for (int j = 0;j < numVerticies(g); j++) {
-            ShortestPaths p = dijkstra(g, i);
-            for (int k = 0; p.pred[k] != NULL; k++) {
-                for (int l = 0; p.pred[k][l].next != NULL; l++) {
-                    if (p.pred[k][l].v == i) {
-                        count2++;
+    NodeValues new = newNode(numVerticies(g));
+    Betweeness bw;
+    for (int v = 0; v < numVerticies(g); v++) {
+        for (int src = 0; src < numVerticies(g); src++) {
+            if (src != v) {
+                ShortestPaths paths = dijkstra(g, src);
+                for (int dest = 0; dest < numVerticies(g); dest++) {
+                    if (src != dest && dest != v) {
+                        bw.counterV = bw.counterP = 0;
+                        bw = countShortestPaths(paths, bw, src, dest, v);
+                        if (bw.counterP != 0) {
+                            new.values[v] = new.values[v] + bw.counterV/bw.counterP;
+                        }
                     }
-                    count1++;
                 }
             }
         }
-        value = count1/count2;
-        betweenness.values[i] = value;
     }
-    return betweenness;
+    return new;
 }
 
+// Calculate normalised betweenness centrality
 NodeValues betweennessCentralityNormalised(Graph g){
     if (g == NULL) {
         NodeValues throwAway = {0};
         return throwAway;
     }
-    NodeValues normalised = newNode(numVerticies(g));   
-    NodeValues bc = betweennessCentrality(g);
-    for (int i = 0; i < numVerticies(g); i++) {
-        normalised.values[i] = bc.values[i]/((numVerticies(g) - 1)*(numVerticies(g) - 2));
-    }
-	return normalised;
+    NodeValues normalised = betweennessCentrality(g);
+    int i = 0;
+    for (i = 0; i < normalised.noNodes; i++) {
+        normalised.values[i] = normalised.values[i]/(numVerticies(g)-1)/(numVerticies(g)-2);
+   }
+   return normalised;
 }
 
+// Print node values list
 void showNodeValues(NodeValues values){
     for (int i = 0; i < values.noNodes; i++){
         printf("%d: %lf\n", i, values.values[i]);
@@ -133,9 +152,22 @@ static NodeValues newNode(int nV){
     NodeValues new;
     new.noNodes = nV;
     new.values = malloc(nV*sizeof(double));
-    assert(new.values != NULL);
     for (int i = 0; i < nV; i++){
         new.values[i] = 0;
     }
     return new;
+}
+
+// The inplementation of counting number of times passing through v and shortest paths tracking and return betweeness data struct
+static Betweeness countShortestPaths(ShortestPaths paths, Betweeness bw, int src, int dest, int v) {
+    PredNode *prev;  
+    for (prev = paths.pred[dest]; prev != NULL; prev = prev->next) {
+        if (prev->v == src) {
+            bw.counterP++;
+        } else if (prev->v == v && prev->v !=src) {
+           bw.counterV++;
+        }
+        bw = countShortestPaths(paths, bw, src, prev->v, v); 
+    }
+    return bw;
 }
